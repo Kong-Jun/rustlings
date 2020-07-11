fa21a02b44bd3062d3f4931c8ce0f30295350ca5
Kong-Jun <kongjun18@outlook.com>
2020-07-12 00:05:38 +0800

    完成了macros练习,宏好难啊

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -4,8 +4,12 @@
 // - Macros
 
 // Write a macro that passes the test! No hints this time, you can do it!
-
-// I AM NOT DONE
+#[macro_export]
+macro_rules! my_macro {
+    ($val: expr) => {
+        "Hello $val"
+    };
+}
 
 #[cfg(test)]
 mod tests {
